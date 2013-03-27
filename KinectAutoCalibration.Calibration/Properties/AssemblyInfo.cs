a20a611097a35676065980132fd35643b85b9d18
Engelbert Luechinger <eluechin@hsr.ch>
2013-03-27 17:33:16 +0100

    Renamed project to KinectCalibration

--- KinectAutoCalibration.Calibration/Properties/AssemblyInfo.cs
+++ KinectAutoCalibration.Calibration/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KinectAutoCalibration.Calibration")]
+[assembly: AssemblyTitle("KinectAutoCalibration.KinectCalibration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KinectAutoCalibration.Calibration")]
+[assembly: AssemblyProduct("KinectAutoCalibration.KinectCalibration")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
