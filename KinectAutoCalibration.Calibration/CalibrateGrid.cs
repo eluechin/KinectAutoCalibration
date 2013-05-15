d530073c5e1ab18ba5e31cdb8c6f7ff65e18cf0c
Engelbert Luechinger <eluechin@hsr.ch>
2013-05-15 09:15:27 +0200

    Refactoring

--- KinectAutoCalibration.Calibration/CalibrateGrid.cs
+++ KinectAutoCalibration.Calibration/CalibrateGrid.cs
@@ -16,7 +16,7 @@ namespace KinectAutoCalibration.Calibration
 
         public Dictionary<BeamerPoint, KinectPoint> CalibrateBeamerToKinect(IBeamerWindow beamerWindow, IKinect kinect)
         {
-            var newPoints = new Dictionary<BeamerPoint, KinectPoint>();
+            //var newPoints = new Dictionary<BeamerPoint, KinectPoint>();
 
             var simpleStrategy = new CalibrateEdgePoints();
             var beamerToKinect = simpleStrategy.CalibrateBeamerToKinect(beamerWindow, kinect);
@@ -26,40 +26,85 @@ namespace KinectAutoCalibration.Calibration
             for (var i = 1; i <= CALIBRATION_ROUNDS; i++)
             {
                 beamerWindow.DisplayCalibrationImage(true, i);
+                Thread.Sleep(KinectBeamerCalibration.THREAD_SLEEP);
                 var picture1 = kinect.GetColorImage();
-                Thread.Sleep(1000);
+                Thread.Sleep(KinectBeamerCalibration.THREAD_SLEEP);
                 beamerWindow.DisplayCalibrationImage(false, i);
+                Thread.Sleep(KinectBeamerCalibration.THREAD_SLEEP);
                 var picture2 = kinect.GetColorImage();
 
                 var diffKinectPoints = kinect.GetDifferenceImage(picture1, picture2, KinectBeamerCalibration.THRESHOLD);
-
-                var initVectors = CalculateInitVectors(i);
                 var diffKinectVectors = KinectPointArrayHelper.ExtractBlackPointsAs2dVector(diffKinectPoints);
 
-                //var initPoints = CalculateInitVectors(beamerToKinect.Values.GetEnumerator(), i);
+                var newPoints = CalculateNewPoints(i, diffKinectVectors);
+
+                //var initPoints = CalculateNewPoints(beamerToKinect.Values.GetEnumerator(), i);
             }
             //TODO Implement
             throw new NotImplementedException();
         }
 
-        private List<Vector2D> CalculateInitVectors(int round)
+        private List<Vector2D> CalculateNewPoints(int round, List<Vector2D> diffKinectVectors)
         {
+            var numberOfAreaHorizontal = (int) Math.Pow(2, round);
+            var numberOfAreaVertical = numberOfAreaHorizontal;
 
-            var initVectors = new List<Vector2D>();
-            var widthDivisor = (int) Math.Pow(2, round);
+            var edgePoints = Calibration.GetEdgePoints();
 
-            for (var i = 1; i <= round; i++)
+
+            for(var i = 1; i<= numberOfAreaHorizontal; i++)
             {
-                for (int j = 0; j < round; j++)
+                for (var j = 1; j <= numberOfAreaVertical; j++)
                 {
                     
                 }
-                initVectors.AddRange(CalculateTopLine(i, widthDivisor));
             }
+            
+            
+            
+            
+            //var edgePoints = Calibration.GetEdgePoints();
+
+
+
+
+
+
+            //var initVectors = new List<Vector2D>();
+            //var widthDivisor = (int) Math.Pow(2, round);
+
+            //for (var i = 1; i <= round; i++)
+            //{
+            //    for (int j = 0; j < round; j++)
+            //    {
+                    
+            //    }
+            //    initVectors.AddRange(CalculateTopLine(i, widthDivisor));
+            //}
 
             
           
-            return initVectors;
+            //return initVectors;
+
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<Vector2D> CalculateInnerGrid(Vector2D topLeft, Vector2D topRight, Vector2D bottomRight, Vector2D bottomLeft)
+        {
+            var innerPoints = new List<Vector2D>
+                {
+                    // top point
+                    topLeft.Add(topRight).Divide(2),
+                    // middle left point
+                    topLeft.Add(bottomLeft).Divide(2),
+                    // middle point
+                    topLeft.Add(bottomRight).Divide(2),
+                    // middle right point
+                    topRight.Add(bottomRight).Divide(2),
+                    // bottom point
+                    bottomLeft.Add(bottomRight).Divide(2)
+                };
+            return innerPoints;
         }
 
         private IEnumerable<Vector2D> CalculateTopLine(int numberOfPoints, int divisor)
