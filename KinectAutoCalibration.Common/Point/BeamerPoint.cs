a0e7b5dbe621943970d062723b32af820f9ad147
Engelbert Luechinger <eluechin@hsr.ch>
2013-05-21 15:27:39 +0200

    Add ToVector2D method to BeamerPoint class

--- KinectAutoCalibration.Common/Point/BeamerPoint.cs
+++ KinectAutoCalibration.Common/Point/BeamerPoint.cs
@@ -4,5 +4,14 @@
     {
         public int X { get; set; }
         public int Y { get; set; }
+
+        /// <summary>
+        /// Convert the BeamerPoint into a Vector 2D
+        /// </summary>
+        /// <returns>Vector 2D Object with the values of this point.</returns>
+        public Vector2D ToVector2D()
+        {
+            return new Vector2D { X = X, Y = Y };
+        }
     }
 }
